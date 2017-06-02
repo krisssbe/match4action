2974e8888f07f5a0c9169675109c7935cf2fafbc
krisssbe <kristin.bredemeier@gmail.com>
2017-06-01 20:52:32 -0700

    latest changes after class; carousel added

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,23 @@
-$('document').ready(function(){
+$(document).ready(function(){
   $('#menu_toggler').click(function(){
     $('#main_menu').toggle();
+  });
+
+  $('.slideshow').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+
+        }
+      }
+    ]
   });
 });
